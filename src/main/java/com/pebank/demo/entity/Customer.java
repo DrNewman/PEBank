19b8a02399133454f0831903734f29a91e0bd112
Oleksandr Makovetskyi <makovetskyi@netcracker.com>
2018-07-19 18:04:32 +0300

    Initial commit

---
+++ src/main/java/com/pebank/demo/entity/Customer.java
@@ -0,0 +1,54 @@
+package com.pebank.demo.entity;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import java.io.Serializable;
+
+@Entity
+public class Customer implements Serializable{
+
+    @Id
+    @GeneratedValue
+    private long id;
+
+    private String firstName;
+
+    private String lastName;
+
+    private String address;
+
+    public Customer() {
+    }
+
+    public Customer(String firstName, String lastName, String address) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.address = address;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+}
